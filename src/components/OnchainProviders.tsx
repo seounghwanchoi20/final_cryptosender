9d4833e5e4f44b433d200bdfe0c9bc9f3346a605
최승환 <robin@172-1-116-54.lightspeed.elpstx.sbcglobal.net>
2024-12-12 21:51:26 -0500

    vercel4

--- src/components/OnchainProviders.tsx
+++ src/components/OnchainProviders.tsx
@@ -1,12 +1,12 @@
-'use client';
-import { OnchainKitProvider } from '@coinbase/onchainkit';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import type { ReactNode } from 'react';
-import { base } from 'viem/chains';
-import { WagmiProvider } from 'wagmi';
-import { NEXT_PUBLIC_CDP_API_KEY } from '../config';
-import { useWagmiConfig } from '../wagmi';
+"use client";
+import { OnchainKitProvider } from "@coinbase/onchainkit";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { ReactNode } from "react";
+import { base } from "viem/chains";
+import { WagmiProvider } from "wagmi";
+import { NEXT_PUBLIC_CDP_API_KEY } from "../config";
+import { useWagmiConfig } from "../wagmi";
 
 type Props = { children: ReactNode };
 
@@ -18,13 +18,16 @@ function OnchainProviders({ children }: Props) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider apiKey={NEXT_PUBLIC_CDP_API_KEY} chain={base}
-         config={{
-          appearance: {
-            mode: 'auto',
-            theme: 'base', 
-          },
-        }}>
+        <OnchainKitProvider
+          apiKey={NEXT_PUBLIC_CDP_API_KEY}
+          chain={base}
+          config={{
+            appearance: {
+              mode: "auto",
+              theme: "base",
+            },
+          }}
+        >
           <RainbowKitProvider modalSize="compact">
             {children}
           </RainbowKitProvider>
