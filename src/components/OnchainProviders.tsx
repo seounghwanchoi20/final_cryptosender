3d73e03b315595dbfa4e892353dd4e69c339e34a
최승환 <robin@172-1-116-54.lightspeed.elpstx.sbcglobal.net>
2024-12-12 22:05:17 -0500

    vercel7

--- src/components/OnchainProviders.tsx
+++ src/components/OnchainProviders.tsx
@@ -1,46 +1,54 @@
 "use client";
 import { OnchainKitProvider } from "@coinbase/onchainkit";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import type { ReactNode } from "react";
-import { base } from "viem/chains";
 import { WagmiProvider } from "wagmi";
-import { NEXT_PUBLIC_CDP_API_KEY } from "../config";
-import { useWagmiConfig } from "../wagmi";
 import type { Chain } from "viem";
 
-type Props = { children: ReactNode };
+const NEXT_PUBLIC_CDP_API_KEY = process.env.NEXT_PUBLIC_CDP_API_KEY;
+
+const baseChain = {
+  id: 8453,
+  name: "Base",
+  network: "base",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://mainnet.base.org"],
+    },
+    public: {
+      http: ["https://mainnet.base.org"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Basescan",
+      url: "https://basescan.org",
+    },
+  },
+} as Chain;
 
 const queryClient = new QueryClient();
 
-const baseChainConfig: Chain = {
-  ...base,
-  id: 8453,
-  name: "Base",
-};
-
-function OnchainProviders({ children }: Props) {
-  const wagmiConfig = useWagmiConfig();
-
+export function OnchainProviders({ children, wagmiConfig }) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <OnchainKitProvider
           apiKey={NEXT_PUBLIC_CDP_API_KEY}
-          chain={baseChainConfig}
+          chain={baseChain}
           config={{
             appearance: {
               mode: "auto",
-              theme: "base",
             },
           }}
         >
-          <RainbowKitProvider modalSize="compact">
-            {children}
-          </RainbowKitProvider>
+          {children}
         </OnchainKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
 }
-export default OnchainProviders;
