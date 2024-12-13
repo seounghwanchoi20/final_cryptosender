66187904512516191dcf7230dfba7ee16f3d650a
최승환 <robin@172-1-116-54.lightspeed.elpstx.sbcglobal.net>
2024-12-12 21:35:59 -0500

    vercel3

--- src/components/IdentityWrapper.tsx
+++ src/components/IdentityWrapper.tsx
@@ -1,9 +1,35 @@
 import { IdentityCard } from "@coinbase/onchainkit/identity";
 import { useAccount } from "wagmi";
-import { base } from "viem/chains";
+
+const baseChain = {
+  id: 8453,
+  name: "Base",
+  network: "base",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://mainnet.base.org"],
+    },
+    public: {
+      http: ["https://mainnet.base.org"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Basescan",
+      url: "https://basescan.org",
+    },
+  },
+};
 
 export default function IdentityWrapper() {
   const { address } = useAccount();
 
-  return <IdentityCard address={address} schemaId={address} chain={base} />;
+  return (
+    <IdentityCard address={address} schemaId={address} chain={baseChain} />
+  );
 }
