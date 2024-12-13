734cefd4a71d7f53dc654fea28fb3bc06679452e
최승환 <robin@172-1-116-54.lightspeed.elpstx.sbcglobal.net>
2024-12-12 21:32:35 -0500

    vercel2

--- src/components/IdentityWrapper.tsx
+++ src/components/IdentityWrapper.tsx
@@ -1,12 +1,9 @@
 import { IdentityCard } from "@coinbase/onchainkit/identity";
-import { base } from "viem/chains";
 import { useAccount } from "wagmi";
-import type { Chain } from "viem";
+import { base } from "viem/chains";
 
 export default function IdentityWrapper() {
   const { address } = useAccount();
 
-  return (
-    <IdentityCard address={address} schemaId={address} chain={base as Chain} />
-  );
+  return <IdentityCard address={address} schemaId={address} chain={base} />;
 }
